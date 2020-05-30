0050bd5122e310f518d071cb909a3ea697abf477
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-05-30 12:10:58 +0000

    build based on 7b3f96f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#","page":"Home","title":"Home","text":"CurrentModule = SciPy","category":"page"},{"location":"#SciPy-1","page":"Home","title":"SciPy","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [SciPy]","category":"page"}]
+}
