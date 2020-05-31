f79b3422dd8d2dfbcbbe5475b3f67887648155a6
Atsushi Sakai <asakai.amsl+github@gmail.com>
2020-05-31 12:36:22 +0900

    add print_configulations code

--- src/SciPy.jl
+++ src/SciPy.jl
@@ -1,18 +1,44 @@
 module SciPy
 
 using PyCall
+using InteractiveUtils
 
+export print_configulations
 export constants
 
 const scipy = PyNULL()
 const constants = PyNULL()
 
-
+"""
+Module initialization function
+"""
 function __init__()
 
     copy!(scipy, pyimport_conda("scipy", "scipy", "conda-forge"))
     copy!(constants, pyimport("scipy.constants"))
 
+end
+
+"""
+Print configulations like:
+- Julia version
+- Python version
+- Python path
+- scipy version
+"""
+function print_configulations()
+    println("==== SciPy.jl configulations ====")
+    println("-- Julia settings --")
+    versioninfo()
+    println("-- PyCall.jl settings --")
+    @show PyCall.pyversion
+    @show PyCall.libpython
+    @show PyCall.pyprogramname
+    @show PyCall.conda
+    println("-- Scipy settings --")
+    @show scipy.__version__
+    @show scipy.version.full_version
+    @show scipy.version.git_revision
 end
 
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,10 @@ using SciPy
 using Test
 
 @testset "SciPy.jl" begin
-    @test constants.golden == 1.618033988749895
+    # Print configulations before start testings.
+    print_configulations()
+
+    @testset "constants" begin
+        @test constants.golden == 1.618033988749895
+    end
 end
