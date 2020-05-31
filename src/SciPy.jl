76ccd0c85abde8fb39b4731709608c21afc76215
Atsushi Sakai <asakai.amsl+github@gmail.com>
2020-05-31 16:23:33 +0900

    update doc

--- src/SciPy.jl
+++ src/SciPy.jl
@@ -28,6 +28,16 @@ You can access each constants:
 ```julia-repl
 julia> SciPy.constants.golden
 1.618033988749895
+
+julia> SciPy.constants.physical_constants["electron mass"] 
+(9.10938356e-31, "kg", 1.1e-38)
+
+julia> SciPy.constants.convert_temperature([-40, 40.0], "Celsius", "Kelvin") 
+
+2-element Array{Float64,1}:
+ 233.14999999999998
+ 313.15
+
 ```
 """
 const constants = PyNULL()
