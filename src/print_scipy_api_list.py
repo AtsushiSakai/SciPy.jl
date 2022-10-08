f77dc701c5192cca11fc8bf1f3a8e6c466b2bbc5
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2022-10-08 19:57:25 +0900

    call print_all from scipy/scipy_api_list.py

--- src/print_scipy_api_list.py
+++ src/print_scipy_api_list.py
@@ -3,23 +3,6 @@
 This is a python script to show API for each submodules of SciPy
 
 """
-import sys, scipy, numpy, json
-print(scipy.__version__, numpy.__version__, sys.version_info)
+from scipy_api_list import print_all
 
-print("=== stats module ===")
-from scipy import stats
-print(json.dumps(stats.__all__))
-
-print("=== optimize module ===")
-from scipy import optimize
-print(json.dumps(optimize.__all__))
-
-print("=== interpolate module ===")
-from scipy import interpolate
-print(json.dumps(interpolate.__all__))
-
-print("=== spatial module ===")
-from scipy import spatial
-print(json.dumps(spatial.__all__))
-
-
+print_all()
