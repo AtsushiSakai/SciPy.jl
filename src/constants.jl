e505485134f1b112f53efe0eb825b679c44585c7
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2022-10-10 01:02:05 +0900

    fix docstring

--- src/constants.jl
+++ src/constants.jl
@@ -15,8 +15,7 @@ julia> SciPy.constants.physical_constants["electron mass"]
 (9.10938356e-31, "kg", 1.1e-38)
 
 julia> SciPy.constants.convert_temperature([-40, 40.0], "Celsius", "Kelvin")
-
-2-element Array{Float64,1}:
+2-element Vector{Float64}:
  233.14999999999998
  313.15
 
